e1ac589a65b01e82bc43d6c29fe1c7f5775249f0
adii21-ux <adityasangave2112@gmail.com>
2024-01-30 18:36:53 +0530

    Feature : Added Delete Videos Support

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('get-videos/', views.getData),
+    path('get-videos/', views.getData, name="get_videos"),
+    path('delete-all-videos/', views.delete_all_videos, name='delete_all_videos'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,7 @@ from rest_framework.response import Response
 from datetime import datetime
 from .serializers import VideoSerializer
 from .models import Video
-# Create your views here.
-
+from rest_framework import status
 
 @api_view(['GET'])
 def getData(request):
@@ -12,3 +11,11 @@ def getData(request):
     serializer = VideoSerializer(videos, many=True)
     return Response(serializer.data)
 
+@api_view(['DELETE'])
+def delete_all_videos(request):
+    try:
+        # Delete all videos from the database
+        Video.objects.all().delete()
+        return Response({'message': 'All videos deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
