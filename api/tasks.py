c0e79135cc203da4018de3b49f5516cc2d4c6868
adii21-ux <adityasangave2112@gmail.com>
2024-01-31 01:44:59 +0530

    Updated celery task

--- api/tasks.py
+++ api/tasks.py
@@ -16,34 +16,30 @@ def format_published_after(datetime_obj):
     return datetime_obj.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 @shared_task
-def fetch_and_store_videos(search_query='minecraft', days_ago=200, max_results=25):
-    # latest_video_published_at = Video.objects.order_by('-published_at').first()
-    # # print(latest_video_published_at.published_at)
-    
-    # if latest_video_published_at:
-    #     # If there are existing videos in the database, set the publishedAfter parameter
-    #     latest_date = str(latest_video_published_at.published_at)
-    #     latest_video_published_at = datetime.fromisoformat(latest_date).replace(tzinfo=None)
-    #     current_datetime = datetime.strptime(str(datetime.utcnow()), "%Y-%m-%d %H:%M:%S.%f") 
-        
-    #     delta_days = (current_datetime - latest_video_published_at).days
-    #     # days_ago = timedelta(days=delta_days)
-    #     # print("delta_days", delta_days)
-    #     # print("days_ago", days_ago)
-    
+def fetch_and_store_videos(search_query='minecraft', days_ago=7, max_results=50):
     # Get the most recent published_at value from the database
-    most_recent_video = Video.objects.order_by('-published_at').first()
-    published_after_datetime = most_recent_video.published_at if most_recent_video else datetime.utcnow() - timedelta(days=7)
+    last_published_at = None
+    most_recent_video = Video.objects.order_by('-published_at').last()
+    if most_recent_video:
+        print(most_recent_video.published_at)
+    published_after_datetime = most_recent_video.published_at if most_recent_video else datetime.utcnow() - timedelta(days=days_ago)
     
     videos_data = []
     
     for api_key in DEVELOPER_KEYS:
-        # Calculate publishedAfter datetime
-        # published_after_datetime = datetime.utcnow() - timedelta(days=delta_days)
-        published_after_string = format_published_after(published_after_datetime)
+        # Set initial values for published_after and published_before
+        if last_published_at is None:
+            published_before = datetime.now()
+            published_after = published_before - timedelta(days=20)
+        else:
+            published_before = last_published_at
+            published_after = published_before - timedelta(days=20)
+
+        # Convert datetime objects to formatted strings
+        published_before_string = format_published_after(published_before)
+        published_after_string = format_published_after(published_after)
+
         
-        print("published_after_string",published_after_string)
-        print("published_after_string",published_after_datetime)
         params = {
             'part': 'snippet',
             'q': search_query,
@@ -51,30 +47,31 @@ def fetch_and_store_videos(search_query='minecraft', days_ago=200, max_results=2
             'type': 'video',
             'maxResults': max_results,
             'publishedAfter': published_after_string,
+            'publishedBefore': published_before_string,
         }
 
         res = requests.get('https://www.googleapis.com/youtube/v3/search', params=params)
         response = res.json()
-        print(response)
+        # print(response)
         
-        if 'error' in response:
-            error_reason = response['error']['errors'][0]['reason']
-            if error_reason == 'rateLimitExceeded':
-                # Move to the next API key if rate limit is exceeded
-                continue
+        if 'error' in response and any(error.get('reason') == 'quotaExceeded' for error in response['error']['errors']):
+            # Move to the next API key if quota is exceeded
+            continue
 
         with transaction.atomic():
             for item in response['items']:
-                video_data = {
-                    'video_id': item['id']['videoId'],
-                    'title': item['snippet']['title'],
-                    'description': item['snippet']['description'],
-                    'published_at': datetime.strptime(item['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ"),
-                    'thumbnail_medium_url': item['snippet']['thumbnails']['medium']['url'],
-                    'channel_id': item['snippet']['channelId'],
-                    'channel_title': item['snippet']['channelTitle'],
-                }
-                videos_data.append(video_data)
+                video_id = item['id']['videoId']
+                if not Video.objects.filter(video_id=video_id).exists():
+                    video_data = {
+                        'video_id': item['id']['videoId'],
+                        'title': item['snippet']['title'],
+                        'description': item['snippet']['description'],
+                        'published_at': datetime.strptime(item['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ"),
+                        'thumbnail_medium_url': item['snippet']['thumbnails']['medium']['url'],
+                        'channel_id': item['snippet']['channelId'],
+                        'channel_title': item['snippet']['channelTitle'],
+                    }
+                    videos_data.append(video_data)
 
             Video.objects.bulk_create([Video(**data) for data in videos_data])
             
